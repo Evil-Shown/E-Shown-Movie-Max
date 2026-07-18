830c92e3b2d84a12978bcf391214d62f342945e1
Damitha Samarakoon <damitharaveesh@gmail.com>
2026-07-19 02:14:10 +0530

    small bug fixes in controllers

--- server/src/domains/tmdb/tmdb.controller.ts
+++ server/src/domains/tmdb/tmdb.controller.ts
@@ -3,16 +3,15 @@ import { tmdbGet } from "../../lib/tmdb";
 
 export async function proxy(req: Request, res: Response, next: NextFunction): Promise<void> {
   try {
-    const tmdbPath = req.path;
-
+    const urlParts = req.url.split("?");
+    const tmdbPath = urlParts[0];
     const params: Record<string, string> = {};
-    const query = req.query as Record<string, unknown>;
-    for (const [key, val] of Object.entries(query)) {
-      if (typeof val === "string") {
-        params[key] = val;
-      } else if (Array.isArray(val)) {
-        params[key] = val[0];
-      }
+
+    if (urlParts[1]) {
+      const usp = new URLSearchParams(urlParts[1]);
+      usp.forEach((value, key) => {
+        params[key] = value;
+      });
     }
 
     const data = await tmdbGet<unknown>(tmdbPath, params);
